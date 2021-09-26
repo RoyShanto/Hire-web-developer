b7e49a4f9989adb84369fa1a3e55d759ab2f1055
Your Name <roycse17@gmail.com>
2021-09-26 18:01:09 +0600

    Assignment 8 done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import './Cart.css';
 const Cart = (props) => {
-    // console.log(props.developers);
     const { Name, Type, Salary, Level, country, img } = props.developers;
-    // const handleToAdd = (name, salary) => {
-    //     console.log(name, salary);
-    // }
     return (
         <div className="cart">
             {/* <h3>Order Summary</h3> */}
             <img src={img} height="200" width="200" alt="" />
             <h1>Name: {Name}</h1>
             <h3>Type: {Type}</h3>
-            <h3>Salary: {Salary}</h3>
+            <h3>Salary: <span>$</span>{Salary}</h3>
             <h3>Level: {Level}</h3>
             <h3>Country: {country}</h3>
             {/* <h5>Type: {total.toFixed(2)}</h5> */}
+            <a href="https://www.linkedin.com/"><i className="fab fa-linkedin icon"></i></a>&nbsp;&nbsp;&nbsp;
             <button className="btn-hire" onClick={() => props.handelAddToCart(props.developers)}> <i className="fas fa-user-check"></i>&nbsp;&nbsp;Hire</button>
+            {/* &nbsp;&nbsp;<i className="fas fa-user-check"></i> */}
         </div >
     );
 };
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,6 +5,7 @@ const Header = () => {
         <div className="header">
             <h1>*** Hire Web Developer ***</h1>
             <h3>We Have 20 + Developers</h3>
+            <h5>Total Budget: {100} Million</h5>
         </div>
     );
 };
